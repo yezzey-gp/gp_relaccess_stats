e69de98c586b1fb6f9f6d428af15011cfb566edf
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2024-03-23 17:32:19 +0300

    Collect relation names at access time

--- sql/gp_relaccess_stats--1.0.sql
+++ sql/gp_relaccess_stats--1.0.sql
@@ -6,7 +6,8 @@
 CREATE TABLE relaccess_stats (
     dbid Oid,
     relid Oid,
-    userid Oid,
+    relname Name,
+    last_user_id Oid,
     last_read timestamp,
     last_write timestamp,
     n_select_queries int,
@@ -26,28 +27,29 @@ LANGUAGE plpgsql VOLATILE EXECUTE ON MASTER AS
 $func$
 BEGIN
     EXECUTE 'DROP TABLE IF EXISTS relaccess_stats_update_tmp';
-    
+
     EXECUTE 'CREATE TEMP TABLE relaccess_stats_update_tmp (LIKE relaccess_stats) distributed by (dbid, relid)';
-    
+
     EXECUTE format('COPY relaccess_stats_update_tmp FROM ''%s'' WITH (FORMAT ''csv'', DELIMITER '','')', $1);
-    
-    EXECUTE 'INSERT INTO relaccess_stats 
-        SELECT dbid, relid, userid, last_read, last_write, 0, 0, 0, 0, 0 FROM relaccess_stats_update_tmp stage 
+
+    EXECUTE 'INSERT INTO relaccess_stats
+        SELECT dbid, relid, relname, last_user_id, last_read, last_write, 0, 0, 0, 0, 0 FROM relaccess_stats_update_tmp stage
         WHERE NOT EXISTS (
             SELECT 1 FROM relaccess_stats orig WHERE orig.dbid = stage.dbid AND orig.relid = stage.relid)';
-    
-    EXECUTE 'UPDATE relaccess_stats orig SET 
-        userid = stage.userid,
+
+    EXECUTE 'UPDATE relaccess_stats orig SET
+        relname = stage.relname,
+        last_user_id = stage.last_user_id,
         last_read = stage.last_read,
         last_write = stage.last_write,
         n_select_queries = orig.n_select_queries + stage.n_select_queries,
         n_insert_queries = orig.n_insert_queries + stage.n_insert_queries,
         n_update_queries = orig.n_update_queries + stage.n_update_queries,
         n_delete_queries = orig.n_delete_queries + stage.n_delete_queries,
-        n_truncate_queries = orig.n_truncate_queries + stage.n_truncate_queries 
+        n_truncate_queries = orig.n_truncate_queries + stage.n_truncate_queries
     FROM relaccess_stats_update_tmp stage
         WHERE orig.dbid = stage.dbid AND orig.relid = stage.relid';
-    
+
     EXECUTE 'DROP TABLE IF EXISTS relaccess_stats_update_tmp';
 END
 $func$;
--- src/gp_relaccess_stats.c
+++ src/gp_relaccess_stats.c
@@ -13,6 +13,7 @@
 #include "storage/spin.h"
 #include "utils/builtins.h"
 #include "utils/datetime.h"
+#include "utils/lsyscache.h"
 #include "utils/memutils.h"
 #include "utils/timestamp.h"
 #include "tcop/utility.h"
@@ -36,6 +37,9 @@ static int relaccess_match_fn(const void *key1, const void *key2, Size keysize);
 static uint32 local_relaccess_hash_fn(const void *key, Size keysize);
 static int local_relaccess_match_fn(const void *key1, const void *key2,
                                     Size keysize);
+static uint32 relnamecache_hash_fn(const void *key, Size keysize);
+static int relnamecache_match_fn(const void *key1, const void *key2,
+                                 Size keysize);
 static bool collect_relaccess_hook(List *rangeTable, bool ereport_on_violation);
 static void relaccess_xact_callback(XactEvent event, void *arg);
 static void collect_truncate_hook(Node *parsetree, const char *queryString,
@@ -44,6 +48,7 @@ static void collect_truncate_hook(Node *parsetree, const char *queryString,
                                   char *completionTag);
 static void relaccess_executor_end_hook(QueryDesc *query_desc);
 static void memorize_local_access_entry(Oid relid, AclMode perms);
+static void update_relname_cache(Oid relid, char *relname);
 
 static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
 static ExecutorCheckPerms_hook_type prev_check_perms_hook = NULL;
@@ -57,6 +62,7 @@ typedef struct relaccessHashKey {
 
 typedef struct relaccessEntry {
   relaccessHashKey key;
+  NameData relname;
   Oid userid;
   TimestampTz last_read;
   TimestampTz last_write;
@@ -83,11 +89,18 @@ typedef struct localAccessEntry {
   AclMode perms;
 } localAccessEntry;
 
+typedef struct relnameCacheEntry {
+  Oid relid;
+  char *relname;
+} relnameCacheEntry;
+
 static relaccessGlobalData *data;
 static HTAB *relaccesses;
-static const int32 RELACCESS_MAX = 200000;
+static const int32 RELACCESS_MAX = 100000;
 static HTAB *local_access_entries = NULL;
-static const int32 LOCAL_HTAB_SZ = 256;
+static const int32 LOCAL_HTAB_SZ = 128;
+static HTAB *relname_cache = NULL;
+static const int32 RELCACHE_SZ = 16;
 static int stmt_counter = 0;
 
 static const char *const DUMP_FILENAME = "relaccess_stats_dump.csv";
@@ -160,6 +173,18 @@ static int local_relaccess_match_fn(const void *key1, const void *key2,
   return (k1->stmt_cnt == k2->stmt_cnt && k1->relid == k2->relid ? 0 : 1);
 }
 
+static uint32 relnamecache_hash_fn(const void *key, Size keysize) {
+  const Oid *k = (const Oid *)key;
+  return hash_uint32((uint32)(*k));
+}
+
+static int relnamecache_match_fn(const void *key1, const void *key2,
+                                 Size keysize) {
+  const Oid *k1 = (const Oid *)key1;
+  const Oid *k2 = (const Oid *)key2;
+  return (*k1 == *k2 ? 0 : 1);
+}
+
 void _PG_init(void) {
   Size size;
   if (Gp_role != GP_ROLE_DISPATCH) {
@@ -189,7 +214,15 @@ void _PG_init(void) {
   ctl.hash = local_relaccess_hash_fn;
   ctl.match = local_relaccess_match_fn;
   local_access_entries =
-      hash_create("Transaction relaccess entries", LOCAL_HTAB_SZ, &ctl,
+      hash_create("Transaction-wide relaccess entries", LOCAL_HTAB_SZ, &ctl,
+                  HASH_ELEM | HASH_FUNCTION | HASH_COMPARE);
+  MemSet(&ctl, 0, sizeof(ctl));
+  ctl.keysize = sizeof(Oid);
+  ctl.entrysize = sizeof(relnameCacheEntry);
+  ctl.hash = relnamecache_hash_fn;
+  ctl.match = relnamecache_match_fn;
+  relname_cache =
+      hash_create("Transaction-wide relation name cache", RELCACHE_SZ, &ctl,
                   HASH_ELEM | HASH_FUNCTION | HASH_COMPARE);
 }
 
@@ -223,6 +256,7 @@ static bool collect_relaccess_hook(List *rangeTable,
     if (requiredPerms &
         (ACL_SELECT | ACL_INSERT | ACL_UPDATE | ACL_DELETE | ACL_TRUNCATE)) {
       memorize_local_access_entry(relid, requiredPerms);
+      update_relname_cache(relid, NULL);
     }
   }
   return true;
@@ -248,6 +282,7 @@ static void collect_truncate_hook(Node *parsetree, const char *queryString,
       Relation rel;
       rel = heap_openrv(rv, AccessExclusiveLock);
       memorize_local_access_entry(rel->rd_id, ACL_TRUNCATE);
+      update_relname_cache(rel->rd_id, pstrdup(rv->relname));
       heap_close(rel, NoLock);
     }
     MemoryContextSwitchTo(oldcontext);
@@ -263,6 +298,20 @@ static void collect_truncate_hook(Node *parsetree, const char *queryString,
 
 #define UPDATE_STAT(lowercase, uppercase)                                      \
   dst_entry->n_##lowercase += (src_entry->perms & ACL_##uppercase ? 1 : 0)
+
+// if there is a better way to cleanup a postgres hashtable
+// w/o recreating it, I didn't find it
+#define CLEAR_HTAB(entryType, hmap, key_name)                                  \
+  {                                                                            \
+    HASH_SEQ_STATUS hash_seq;                                                  \
+    entryType *src_entry;                                                      \
+    hash_seq_init(&hash_seq, hmap);                                            \
+    while ((src_entry = hash_seq_search(&hash_seq)) != NULL) {                 \
+      bool found;                                                              \
+      hash_search(hmap, &src_entry->key_name, HASH_REMOVE, &found);            \
+      Assert(found);                                                           \
+    }                                                                          \
+  }
 
 static void relaccess_xact_callback(XactEvent event, void * /*arg*/) {
   if (Gp_role != GP_ROLE_DISPATCH) {
@@ -305,22 +354,20 @@ static void relaccess_xact_callback(XactEvent event, void * /*arg*/) {
         dst_entry->last_write =
             Max(GetCurrentTimestamp(), dst_entry->last_write);
       }
+      relnameCacheEntry *relcache_entry = (relnameCacheEntry *)hash_search(
+          relname_cache, &key.relid, HASH_ENTER_NULL, &found);
+      Assert(relcache_entry);
+      strcpy(dst_entry->relname.data, relcache_entry->relname);
+      int namelen = strlen(relcache_entry->relname);
+      dst_entry->relname.data[namelen] = 0;
       LWLockRelease(data->relaccess_ht_lock);
       hash_search(local_access_entries, &src_entry->key, HASH_REMOVE, &found);
       Assert(found);
     }
   } else if (event == XACT_EVENT_ABORT) {
-    // if there is a better way to cleanup a hashtable w/o recreating it, I
-    // didn't find it
-    HASH_SEQ_STATUS hash_seq;
-    localAccessEntry *src_entry;
-    hash_seq_init(&hash_seq, local_access_entries);
-    while ((src_entry = hash_seq_search(&hash_seq)) != NULL) {
-      bool found;
-      hash_search(local_access_entries, &src_entry->key, HASH_REMOVE, &found);
-      Assert(found);
-    }
+    CLEAR_HTAB(localAccessEntry, local_access_entries, key);
   }
+  CLEAR_HTAB(relnameCacheEntry, relname_cache, relid);
 }
 
 Datum relaccess_stats_update(PG_FUNCTION_ARGS) {
@@ -364,10 +411,10 @@ static void relaccess_dump_to_file() {
     read_time_buf[MAXDATELEN] = 0;
     write_time_buf[MAXDATELEN] = 0;
     appendStringInfo(
-        &entry_csv_line, "%d,%d,%d,\"%s\",\"%s\",%ld,%ld,%ld,%ld,%ld\n",
-        entry->key.dbid, entry->key.relid, entry->userid, read_time_buf,
-        write_time_buf, entry->n_select, entry->n_insert, entry->n_update,
-        entry->n_delete, entry->n_truncate);
+        &entry_csv_line, "%d,%d,\"%s\",%d,\"%s\",\"%s\",%ld,%ld,%ld,%ld,%ld\n",
+        entry->key.dbid, entry->key.relid, entry->relname.data, entry->userid,
+        read_time_buf, write_time_buf, entry->n_select, entry->n_insert,
+        entry->n_update, entry->n_delete, entry->n_truncate);
     if (fwrite(entry_csv_line.data, 1, entry_csv_line.len, file) !=
         entry_csv_line.len) {
       hash_seq_term(&hash_seq);
@@ -402,6 +449,37 @@ static void recover_leftover_dump() {
   relaccess_upsert_from_file();
   FreeFile(f);
   unlink(DUMP_FILENAME);
+}
+
+static void update_relname_cache(Oid relid, char *relname) {
+  bool found;
+  relnameCacheEntry *relname_entry = (relnameCacheEntry *)hash_search(
+      relname_cache, &relid, HASH_ENTER, &found);
+  if (!found) {
+    relname_entry->relid = relid;
+    if (!relname) {
+      MemoryContext oldcontext;
+      oldcontext = MemoryContextSwitchTo(TopTransactionContext);
+      relname_entry->relname = get_rel_name(relid);
+      MemoryContextSwitchTo(oldcontext);
+    } else {
+      relname_entry->relname = relname;
+    }
+  } else {
+    /**
+     * NOTE: as we don't handle the 'else' clause here, there will be cases when
+     * we write outdated table names, like below:
+     *    BEGIN;
+     *      INSERT INTO tbl VALUES (1);
+     *      ALTER TABLE tbl RENAME TO new_tbl;
+     *      SELECT * FROM new_tbl;
+     *    COMMIT;
+     * In this case both INSERT and SELECT stmts would be counted with the
+     * old'tbl' name, as we don't update our cache for already known relids in
+     * the same transaction. This is a deliberate decision for performance
+     * reasons.
+     */
+  }
 }
 
 static void memorize_local_access_entry(Oid relid, AclMode perms) {
